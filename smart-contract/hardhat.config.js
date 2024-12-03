22f320a0ac55c0a7dc3188b4d3d68a1b69000faf
Stanislav <staska.golka@gmail.com>
2024-12-03 02:39:42 +0200

    Deployed contract

--- smart-contract/hardhat.config.js
+++ smart-contract/hardhat.config.js
@@ -1,6 +1,19 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
+require("ethers");
+
+const { API_URL, PRIVATE_KEY } = process.env;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
+  defaultNetwork: "sepolia",
+  networks: {
+    hardhat: {},
+    sepolia: {
+      url: API_URL,
+      accounts: [`0x${PRIVATE_KEY}`]
+    }
+  },
 };
+
--- smart-contract/scripts/deploy.js
+++ smart-contract/scripts/deploy.js
@@ -1,14 +1,14 @@
 const hre = require("hardhat");
 
 async function main() {
-  const InsuranceNFT = await hre.ethers.getContractFactory("InsuranceNFT");
+  const InsuranceNFT = await ethers.getContractFactory("InsuranceNFT");
   const contract = await InsuranceNFT.deploy();
-  await contract.deployed();
-
   console.log("Contract deployed to:", contract.address);
 }
 
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
